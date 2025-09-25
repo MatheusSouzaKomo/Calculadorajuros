464e37ff794409aa4533cb7d89eea760771c40b8
Matheus Souza Komo <matheus.sbs03@gmail.com>
2025-09-25 16:29:00 -0300

    Commit atribuido calculadora de juros

--- index.php
+++ index.php
@@ -8,7 +8,34 @@
     <title> Calculadora de Juros</title>
 </head>
 <body>
-    <?php
+<h1>Calculadora de Juros Simples e Conversor de C° para F°</h1>
+<form method="post">
+    Qual das opções quer usar?<br>
+    <input type="radio" name="opcao" value="1" id="c"> 
+    <label for="c">Conversor de C° para F°</label><br>
+
+    <input type="radio" name="opcao" value="2" id="j"> 
+    <label for="j">Calculadora de Juros</label><br><br>
+
+    <select name="operacao" class="operacoes">
+        <option value="capital">Descobrir capital</option>
+        <option value="taxa">Descobrir taxa</option>
+        <option value="tempo">Descobrir tempo</option>
+        <option value="juros">Calcular juros</option>
+    </select><br><br>
+
+    Número 1 (C° ou capital):
+    <input type="number" name="num" required><br><br>
+
+    Número 2 (multiplicador ou tempo em meses):
+    <input type="number" name="num2"><br><br>
+
+    Taxa de juros (%):
+    <input type="number" name="taxa" step="0.01"><br><br>
+
+    <input type="submit" value="Calcular">
+</form>
+ <?php
         $tipo = $_POST['tipo'] ?? '';
         if($_POST){
             if($tipo == "cf" && isset($_POST['valor'])){
@@ -25,45 +52,26 @@
     ?>
     <?php
         if($_POST){
-            $c = $_POST['c'];
-            $t = $_POST['t'];
-            $i = $_POST['i'];
+            $c = $_POST['num'];
+            $t = $_POST['num2'];
+            $i = $_POST['taxa'];
             $j = $_POST['j'];
+            $m = $_POST['montante'];
 
 
             $j = $c * ($i/100) * $t;
-            echo "O valor dos juros é: R$ ".number_format($j, 2, ",", ".");
-
+            echo " O valor dos juros é: R$ ".number_format($j, 2, ",", "." )."<br>";
             $c = $j / (($i/100) * $t);
-            echo "O valor do capital é: R$ ".number_format($c, 2, ",", ".");
+            echo " O valor do capital é: R$ ".number_format($c, 2, ",", ".")."<br>" ;
 
             $i = ($j / ($c * $t)) * 100;
-            echo "O valor da taxa é: ".number_format($i, 2, ",", ".")."%";
+            echo " O valor da taxa é: ".number_format($i, 2, ",", ".")."%"."<br>";
 
             $t = $j / ($c * ($i/100));
-            echo "O valor do tempo é: ".number_format($t, 2, ",", ".")." meses";
+            echo " O valor do tempo é: ".number_format($t)." meses"."<br>";
+
+            $m = $c + $j;
+            echo " O valor do montante é: R$ ".number_format($m, 2, ",", ".")."<br>";
         }
     ?>
-<form method="post">
-
-    Qual das opções quer usar?<br>
-<div>
-    <input type="radio" name="opção" value="1"> Conversor de C° para F°<br>
-    <input type="radio" name="opção" value="2"> Calculadora de Juros<br><br>
-</div> 
-        <select name="operacao" id="operacao">
-            <option value="3">Descobrir capital</option>
-            <option value="4">Descobrir Taxa</option>
-            <option value="5">Descobrir tempo</option>
-            <option value="6">Calcular Juros</option>
-        </select>
-        <br>
-        <br>
-        Digite um número:
-        <input type="number" name="num" required>
-        <input type="submit" value="Calcular">
-        <br>
-        Digite o multiplicador:
-        <input type="number" name="num2" required>
-    </form>
-</body>
+</html>
